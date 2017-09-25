245641eba2947ca72404b4826e30db7fa15f60d5
Emily Morgan <emorgan05@gmail.com>
2017-09-25 17:41:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, data_hash|
-    season.capitalize!
+    season.to_s.capitalize!
     puts "#{season}:"
     data_hash.each do |holiday, array|
       puts "#{holiday}: " + array.each { |supply| puts "supply" }
